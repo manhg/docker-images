1c520f2e407fc4e2672804178ba6161a6a8e3fb9
Giang Manh <manhg@users.noreply.github.com>
2017-08-21 16:34:22 +0900

    Updates

--- adminer/adminer/index.php
+++ adminer/adminer/index.php
@@ -4,7 +4,7 @@ function adminer_object() {
     require __DIR__. '/extra.php';
     $plugins = [
         new AdminerDumpJson(),
-        new AdminerSqlLog(__DIR__. '/../sql_logs'),
+        # new AdminerSqlLog(__DIR__. '/../sql_logs'),
         new AdminerRestoreMenuScroll(),
         new AdminerTablesFilter(),
     ];
