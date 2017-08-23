1053197145b6596a35ac9f2cb958246bc7a6dcdd
Giang Manh <manhg@users.noreply.github.com>
2017-08-23 15:28:18 +0900

    Customize config

--- postgres_replica/10-config.sh
+++ postgres_replica/10-config.sh
@@ -5,6 +5,5 @@ echo [*] configuring $REPLICATION_ROLE instance
 
 # We set master replication-related parameters for both slave and master,
 # so that the slave might work as a primary after failover.
-cat /tmp/replica.conf >> "$PGDATA/postgresql.conf"
-
+echo "include_dir '/var/lib/postgresql/postgresql.conf.d'" >> "$PGDATA/postgresql.conf"
 echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
