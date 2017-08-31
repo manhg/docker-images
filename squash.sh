2b307215788087ac8da552a35d7c672bede41cda
Giang Manh <manhg@users.noreply.github.com>
2017-08-31 11:24:28 +0900

    Updates

--- squash.sh
+++
@@ -1,8 +0,0 @@
-function docker-squash-grep {
-    image_ids=$(docker images | awk /$1/ | awk '{ print $3 }')
-    for image_id in $image_ids
-    do
-        echo "$image_id\n"
-        docker save $image_id | docker-squash -verbose | docker load
-    done
-}
